bd5680c5bab1c5c67adee60f84064ab62d35ee43
ALKAGroup <info@alkavisuals.nl>
2026-09-18 11:19:49 +0200

    Include R2 invoice storage migration in Neon tests

--- scripts/run-neon-test-migrations.mjs
+++ scripts/run-neon-test-migrations.mjs
@@ -63,6 +63,7 @@ const migrations = [
   new URL('../server/db/migrations/015_add_v3_invoice_pdf_storage_binding.sql', import.meta.url),
   new URL('../server/db/migrations/016_create_v3_invoice_access_audit.sql', import.meta.url),
   new URL('../server/db/migrations/017_grant_v3_invoice_access_audit_runtime.sql', import.meta.url),
+  new URL('../server/db/migrations/018_allow_cloudflare_r2_v3_invoice_pdf_storage.sql', import.meta.url),
 ];
 
 const client = await createDefaultNeonClient(migrationUrl);
